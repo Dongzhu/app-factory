136769b14f18da241aa5b0c5cb5bc04906901f1c
Dongzhu <tony@idongzhu.com>
2022-12-28 12:37:22 +0800

    Built the project and modules structure and directories. built passed. 初步构建项目和模块结构和目录结构，项目整体build通过。

---
+++ src/main/java/com/acloud/appfactory/AppFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.acloud.appfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppFactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acloud/appfactory/AppFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acloud.appfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
